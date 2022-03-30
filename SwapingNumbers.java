681e21b7dce6082c88dbef3974402623c05a335e
riya1719 <101990431+riya1719@users.noreply.github.com>
2022-03-30 12:08:38 +0530

    Update SwapingNumbers.java

--- SwapingNumbers.java
+++ SwapingNumbers.java
@@ -5,16 +5,13 @@ public class SwapingWithout3Var {
 		int a = 5;
 		int b = 7;
 		
-		System.out.println("Before Swaping a = " + a+ "b = " +b);
-		
-		a = a+b;// 5+7 =12; a=12
-		b = a-b; // 12- 7 = 5 ; b = 5
-		a = a-b; // 12 -5; a =7;
-		
-		System.out.println("After Swaping a = " + a+ "b = " +b);
-
-		
-
+		System.out.println("Before Swaping a = " + a+ " & b = " +b);
+			
+			a = a+b;// 5+7 =12; a=12
+			b = a-b; // 12- 7 = 5 ; b = 5
+			a = a-b; // 12 -5; a =7;
+			
+			System.out.println("After Swaping a = " + a+ " & b = " +b);
 	}
 
 }
