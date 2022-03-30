7d8bccdc627cc8e2af38be1d3885f86f4ed46920
riya1719 <101990431+riya1719@users.noreply.github.com>
2022-03-30 12:06:00 +0530

    Update SwapingNumbers.java

--- SwapingNumbers.java
+++ SwapingNumbers.java
@@ -1,8 +1,19 @@
 
-public class SwapingNumbers {
+public class SwapingWithout3Var {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		
+		System.out.println("Before Swaping a = " + a+ "b = " +b);
+		
+		a = a+b;// 5+7 =12; a=12
+		b = a-b; // 12- 7 = 5 ; b = 5
+		a = a-b; // 12 -5; a =7;
+		
+		System.out.println("After Swaping a = " + a+ "b = " +b);
+
+		
 
 	}
 
