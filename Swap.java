a0c22f28829ec7ea296ae977ab194845e025469b
riya1719 <101990431+riya1719@users.noreply.github.com>
2022-04-07 12:11:28 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -9,10 +9,12 @@ public static void main(String[] args) {
 		
 		System.out.println("Before Swap: num1 = " + num1 + ", num2 = " + num2);
 		
-		num1 = num1 + num2;
-		num2 = num1 - num2;
-		num1 = num1 - num2;
-		
+		int temp ; // temporary variable
+			
+			temp = num1;
+			num1 =num2;
+			num2 =temp;
+
 		System.out.println("After Swap: num1 = " + num1 + ", num2 = " + num2);
 		
 	}
